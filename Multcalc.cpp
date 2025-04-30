01426f0441bb35e466099909ca236e0411205e35
MFarhan2702 <muhfar2983@gmail.com>
2025-04-30 22:38:16 +0700

    Update untuk bisa kali 3 nomor

--- Multcalc.cpp
+++ Multcalc.cpp
@@ -3,9 +3,11 @@
 int main(){
     int a;
     int b;
+    int d;
     scanf("%d/",&a);
     scanf("%d/",&b);
-    int c = a*b;
+    scanf("%d/",&d);
+    int c = a*b*d;
     printf("%d",c);
     return 0;
 }
